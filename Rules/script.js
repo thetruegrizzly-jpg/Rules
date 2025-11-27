f0084483fb5ffc16b3a277119f6708b643efbb38
thetruegrizzly-jpg <thetruegrizzly84@gmail.com>
2025-11-27 12:41:32 -0500

    Update script.js

--- Rules/script.js
+++ Rules/script.js
@@ -1,1 +1,24 @@
+const hex = document.getElementById("hex");
+const still = document.getElementById("slimeStill");
+const gif = document.getElementById("slimeGif");
 
+hex.addEventListener("mouseenter", () => {
+  hex.classList.add("hovered");
+});
+
+hex.addEventListener("transitionend", (e) => {
+  if (e.propertyName === "transform") {
+    gif.src = "images/Slime gif.gif";
+    gif.style.opacity = 1;
+    still.style.opacity = 0;
+  }
+});
+
+hex.addEventListener("mouseleave", () => {
+  hex.classList.remove("hovered");
+
+  gif.style.opacity = 0;
+  still.style.opacity = 1;
+
+  gif.src = "";
+});
